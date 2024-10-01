e1193807ce9a698efe69e4fa47aae54002c335d2
Rachel <rachelcheework@gmail.com>
2024-10-02 03:18:41 +0800

    h1 element in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
+//import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 import Weather from './weather';
 
 function App() {
   return (
    <Router>
-      <Routes>
-        <Route path="/" exact element={<Weather/>}/>
-      </Routes>
+  {/* <Router basename='/weather-app'> */}
+     <div className='app-container'>
+      <h1> welcome to my weather app</h1>
+        <Routes>
+          <Route path="/" exact element={<Weather/>}/>
+        </Routes>
+      </div>
    </Router>
   );
 }
