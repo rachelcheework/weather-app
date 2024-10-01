1ea403e952930da0d598fc68fe7faa9bad5b66b7
Rachel <rachelcheework@gmail.com>
2024-10-02 03:03:08 +0800

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Weather from './weather';
 
 function App() {
