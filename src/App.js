53895a5d1edb92aa3f3a19ba9f776ca899fd0162
Rachel <rachelcheework@gmail.com>
2024-10-02 18:40:11 +0800

    deploying to github pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-//import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 import Weather from './weather';
 
 function App() {
   return (
-   <Router>
-  {/* <Router basename='/weather-app'> */}
+  <Router basename='/weather-app'>
         <Routes>
           <Route path="/" exact element={<Weather/>}/>
         </Routes>
